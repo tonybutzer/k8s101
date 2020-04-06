86dc7e1bc5d922a7c84f35da76b25be820ac29de
tonybutzer <tonybutzer@gmail.com>
2020-04-06 21:04:35 +0000

    automatic git update from Makefile

---
+++ tfinfra/chs-k3s-rancher/ship.tf
@@ -0,0 +1,53 @@
+/* this is a multi
+comment */
+
+provider "aws" {
+  region = "${var.region}"
+}
+
+/*
+  NAT Instance
+*/
+
+resource "aws_instance" "master" {
+
+  ami           = "${var.ami}"
+
+  instance_type = "${var.ship_instance_type}"
+  key_name                    = "${var.key_name}"
+  subnet_id                   = "${var.subnet_id}"
+  tags = {
+    Name = "butzer-master-rancher"
+    Owner = "butzer@contractor.usgs.gov"
+    Project = "LPIP"
+  }
+  iam_instance_profile                    ="${var.iam_role}"
+
+  security_groups = ["${var.security_group_ssh}", "${var.security_group_ping}", "${var.security_group_web}"]
+  root_block_device {volume_size = 40}
+
+  user_data                   = "${file("files/master-rancher.sh")}"
+
+}
+
+resource "aws_instance" "ship" {
+
+  ami           = "${var.ami}"
+
+  instance_type = "${var.ship_instance_type}"
+  key_name                    = "${var.key_name}"
+  subnet_id                   = "${var.subnet_id}"
+  tags = {
+    Name = "${var.ship_name[count.index]}"
+    Owner = "butzer@contractor.usgs.gov"
+    Project = "LPIP"
+  }
+  iam_instance_profile                    ="${var.iam_role}"
+
+  security_groups = ["${var.security_group_ssh}", "${var.security_group_ping}", "${var.security_group_web}"]
+  root_block_device {volume_size = 40}
+
+  user_data                   = "${file("files/${var.ship_userdata[count.index]}")}"
+
+  count = 2
+}
