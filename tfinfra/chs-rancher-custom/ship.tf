3e13fdef8493f89c6331ca3392dc921002fdc5ce
tonybutzer <tonybutzer@gmail.com>
2020-04-06 14:50:12 +0000

    Adding .gitignore file

---
+++ tfinfra/chs-rancher-custom/ship.tf
@@ -0,0 +1,32 @@
+/* this is a multi
+comment */
+
+provider "aws" {
+  region = "${var.region}"
+}
+
+/*
+  NAT Instance
+*/
+
+
+resource "aws_instance" "ship" {
+
+  ami           = "${var.ami}"
+
+  instance_type = "${var.master_instance_type}"
+  key_name                    = "${var.key_name}"
+  subnet_id                   = "${var.subnet_id}"
+  tags = {
+    Name = "butzer-k8s-sb"
+    Owner = "butzer@contractor.usgs.gov"
+    Project = "LPIP"
+  }
+  iam_instance_profile                    ="${var.iam_role}"
+  # security_groups = ["${var.security_group_ssh}", "${var.security_group_ping}", "${aws_security_group.xrdp.id}"]
+  security_groups = ["${var.security_group_ssh}", "${var.security_group_ping}", "${var.security_group_web}"]
+  root_block_device {volume_size = 40}
+  user_data                   = "${file("files/os_boot.sh")}"
+
+  count = 2
+}
